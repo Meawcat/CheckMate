0251297614e153d2ef887ece6868757c9c133449
hyhan <hhy33719@gmail.com>
2024-05-18 21:43:21 +0900

    gui 수정

--- gui/add_dialog.py
+++ gui/add_dialog.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'add_dialogfrkcVg.ui'
+## Form generated from reading UI file 'add_dialogIXwZzr.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -17,7 +17,8 @@ class Ui_add_dialog(object):
     def setupUi(self, add_dialog):
         if not add_dialog.objectName():
             add_dialog.setObjectName(u"add_dialog")
-        add_dialog.resize(551, 379)
+        add_dialog.resize(550, 400)
+        add_dialog.setMaximumSize(QSize(16777215, 16777215))
         add_dialog.setStyleSheet(u"QDialog{\n"
 "	background-color: #fff;\n"
 "}\n"
@@ -33,9 +34,14 @@ class Ui_add_dialog(object):
 "	background-color:rgba(86,101,115,0.5);\n"
 "	color:  #fff;\n"
 "}")
+        self.horizontalLayout = QHBoxLayout(add_dialog)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.verlay2 = QVBoxLayout()
+        self.verlay2.setObjectName(u"verlay2")
+        self.verlay1 = QVBoxLayout()
+        self.verlay1.setObjectName(u"verlay1")
         self.item_label = QLabel(add_dialog)
         self.item_label.setObjectName(u"item_label")
-        self.item_label.setGeometry(QRect(20, 10, 505, 60))
         sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -47,9 +53,11 @@ class Ui_add_dialog(object):
         font.setBold(False)
         font.setWeight(50)
         self.item_label.setFont(font)
+
+        self.verlay1.addWidget(self.item_label, 0, Qt.AlignHCenter)
+
         self.item_name = QTextEdit(add_dialog)
         self.item_name.setObjectName(u"item_name")
-        self.item_name.setGeometry(QRect(130, 70, 300, 30))
         sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
@@ -58,47 +66,73 @@ class Ui_add_dialog(object):
         self.item_name.setMaximumSize(QSize(300, 30))
         self.item_name.setLineWidth(0)
         self.item_name.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+
+        self.verlay1.addWidget(self.item_name, 0, Qt.AlignHCenter)
+
+
+        self.verlay2.addLayout(self.verlay1)
+
+        self.horlay1 = QHBoxLayout()
+        self.horlay1.setObjectName(u"horlay1")
+        self.file_label = QLabel(add_dialog)
+        self.file_label.setObjectName(u"file_label")
+        sizePolicy.setHeightForWidth(self.file_label.sizePolicy().hasHeightForWidth())
+        self.file_label.setSizePolicy(sizePolicy)
+        self.file_label.setMaximumSize(QSize(16777215, 30))
+        self.file_label.setFont(font)
+
+        self.horlay1.addWidget(self.file_label)
+
+        self.file_button = QPushButton(add_dialog)
+        self.file_button.setObjectName(u"file_button")
+        font1 = QFont()
+        font1.setFamily(u"\ub9d1\uc740 \uace0\ub515")
+        font1.setPointSize(10)
+        self.file_button.setFont(font1)
+        self.file_button.setStyleSheet(u"")
+
+        self.horlay1.addWidget(self.file_button)
+
+
+        self.verlay2.addLayout(self.horlay1)
+
         self.scrollArea = QScrollArea(add_dialog)
         self.scrollArea.setObjectName(u"scrollArea")
-        self.scrollArea.setGeometry(QRect(11, 174, 531, 131))
         self.scrollArea.setStyleSheet(u"background-color: transparent;\n"
 "")
         self.scrollArea.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 526, 129))
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 524, 162))
         self.scrollAreaWidgetContents.setMaximumSize(QSize(526, 166))
+        self.verticalLayout = QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout.setObjectName(u"verticalLayout")
         self.selected = QLabel(self.scrollAreaWidgetContents)
         self.selected.setObjectName(u"selected")
-        self.selected.setGeometry(QRect(10, 10, 511, 111))
         self.selected.setStyleSheet(u"")
-        self.scrollArea.setWidget(self.selected)
-        self.button_box = QPushButton(add_dialog)
-        self.button_box.setObjectName(u"button_box")
-        self.button_box.setGeometry(QRect(210, 320, 120, 41))
-        self.button_box.setMaximumSize(QSize(120, 16777215))
-        font1 = QFont()
-        font1.setFamily(u"\ub9d1\uc740 \uace0\ub515")
-        font1.setPointSize(12)
-        self.button_box.setFont(font1)
-        self.button_box.setContextMenuPolicy(Qt.NoContextMenu)
-        self.button_box.setLayoutDirection(Qt.LeftToRight)
-        self.button_box.setStyleSheet(u"")
-        self.file_label = QLabel(add_dialog)
-        self.file_label.setObjectName(u"file_label")
-        self.file_label.setGeometry(QRect(20, 120, 409, 30))
-        sizePolicy.setHeightForWidth(self.file_label.sizePolicy().hasHeightForWidth())
-        self.file_label.setSizePolicy(sizePolicy)
-        self.file_label.setMaximumSize(QSize(16777215, 30))
-        self.file_label.setFont(font)
-        self.file_button = QPushButton(add_dialog)
-        self.file_button.setObjectName(u"file_button")
-        self.file_button.setGeometry(QRect(430, 116, 111, 41))
+
+        self.verticalLayout.addWidget(self.selected)
+
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+
+        self.verlay2.addWidget(self.scrollArea)
+
+        self.add_button = QPushButton(add_dialog)
+        self.add_button.setObjectName(u"add_button")
+        self.add_button.setMaximumSize(QSize(120, 16777215))
         font2 = QFont()
         font2.setFamily(u"\ub9d1\uc740 \uace0\ub515")
-        font2.setPointSize(10)
-        self.file_button.setFont(font2)
-        self.file_button.setStyleSheet(u"")
+        font2.setPointSize(12)
+        self.add_button.setFont(font2)
+        self.add_button.setContextMenuPolicy(Qt.NoContextMenu)
+        self.add_button.setLayoutDirection(Qt.LeftToRight)
+        self.add_button.setStyleSheet(u"")
+
+        self.verlay2.addWidget(self.add_button, 0, Qt.AlignHCenter)
+
+
+        self.horizontalLayout.addLayout(self.verlay2)
+
 
         self.retranslateUi(add_dialog)
 
@@ -113,9 +147,9 @@ class Ui_add_dialog(object):
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:'Gulim'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'\ub9d1\uc740 \uace0\ub515';\"><br /></p></body></html>", None))
-        self.selected.setText("")
-        self.button_box.setText(QCoreApplication.translate("add_dialog", u"\ud655\uc778", None))
         self.file_label.setText(QCoreApplication.translate("add_dialog", u"\ucd94\uac00\ud560 \ub370\uc774\ud130 \ud30c\uc77c\ub4e4\uc744 \uc120\ud0dd\ud574 \uc8fc\uc138\uc694", None))
         self.file_button.setText(QCoreApplication.translate("add_dialog", u"\ud30c\uc77c \ucc3e\uae30", None))
+        self.selected.setText("")
+        self.add_button.setText(QCoreApplication.translate("add_dialog", u"\ud655\uc778", None))
     # retranslateUi
 
